c09d67dd4593882c45f01499806d27dafe0273b8
Sajal Rajoria <65611049+SajalRajoria@users.noreply.github.com>
2024-10-13 11:24:15 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+     double a,b;
+    printf("Enter the value of a :");
+    scanf("%lf",&a);
+    printf("Enter the value of b : ");
+    scanf("%lf",&b);
+    printf("%lf",a*b);
+    return 0;
+}
